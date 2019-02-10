52cb736b761392fb3c50c8db24c4d449153bc295
Kanrya <nyataraxis@gmail.com>
2019-02-10 09:02:05 +0300

    added features to map via button

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -3,13 +3,13 @@ import UsersContainer from "./container/UsersContainer.jsx";
 import UsersMapContainer from "./container/UsersMapContainer.jsx";
 
 const App = () => (
-  <div className="row mt-5">
-    <div className="col-md-4 offset-md-1">
+  <div className="row mt-1">
+    <div className="col-md-4">
       <h2>Users</h2>
       <UsersContainer />
     </div>
 
-    <div className="col-md-4 offset-md-1">
+    <div className="col-md-8">
       <UsersMapContainer />
     </div>
   </div>
--- src/js/components/container/UsersMapContainer.jsx
+++ src/js/components/container/UsersMapContainer.jsx
@@ -1,22 +1,59 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import "ol/ol.css";
+import { Map, View } from "ol";
+import { Tile as TileLayer, Vector as VectorLayer } from "ol/layer.js";
+import OSM from "ol/source/OSM";
+import GeoJSON from "ol/format/GeoJSON.js";
+import { Vector as VectorSource } from "ol/source.js";
 
 export class UsersMapContainer extends Component {
   constructor() {
     super();
+    this.handleRefresh = this.handleRefresh.bind(this);
   }
   componentDidMount() {
+    this.map = new Map({
+      target: "map",
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        })
+      ],
+      view: new View({
+        center: [0, 0],
+        zoom: 0
+      })
+    });
+    this.vectorSource = new VectorSource({
+      features: new GeoJSON().readFeatures(this.props.features)
+    });
+    this.vectorLayer = new VectorLayer({ source: this.vectorSource });
+    this.map.addLayer(this.vectorLayer);
+  }
+  handleRefresh() {
+      console.log('refresh');
+    this.vectorSource = new VectorSource({
+      features: new GeoJSON().readFeatures(this.props.features)
+    });
+    this.vectorLayer.setSource(this.vectorSource);
   }
   render() {
     return (
-        <div id="map"></div>
+      <div>
+        <div id="map" />
+        <button onClick={this.handleRefresh} />
+      </div>
     );
   }
 }
 
 function mapStateToProps(state) {
   return {
-    users: state.users.slice(0, 10)
+    features: {
+      type: "FeatureCollection",
+      features: state.users.slice(0, 10)
+    }
   };
 }
 export default connect(
